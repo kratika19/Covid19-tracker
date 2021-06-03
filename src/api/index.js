ac94504db14c052a70b4a91b43b70a0a3c007097
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-06-03 22:38:42 +0530

    implemented cards

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,26 @@
+import axios from 'axios';
 import Axios from 'axios'
 
 const url = 'https://covid19.mathdro.id/api';
 
 export const getData = async () => {
     try {
-        var { data: { confirmed, recovered, deaths, lastUpdate } } = await Axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await Axios.get(url);
         var modifiedData = { confirmed, recovered, deaths, lastUpdate }
+        return modifiedData;
+    } catch (error) {
+
+    }
+}
+
+export const getDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`)
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }))
         return modifiedData;
     } catch (error) {
 
--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getDailyData } from '../../api/index'
+import { Line, Bar } from 'react-chartjs-2'
+import styles from './Charts.module.css'
 
 const Charts = () => {
+    const [dailyData, setdailyData] = useState({})
+
+    useEffect(() => {
+        const fetchDailyData = async () => {
+            const Ddata = await getDailyData();
+            setdailyData(Ddata);
+        };
+
+        console.log(dailyData)
+        fetchDailyData();
+    },[]);
+
+    const lineChart = (
+        dailyData.length ? (<Line
+            data={{
+                labels: dailyData.map(({ date }) => date),
+                datasets: [{
+                    data: dailyData.map(({ confirmed }) => confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: true,
+                }, {
+                    data: dailyData.map(({ deaths }) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255,0,0,0.5)',
+                    fill: true,
+                }],
+            }}
+        />) : null
+    );
+
     return (
-        <div>
-            <h1>Charts</h1>
+        <div className={styles.container}>
+            {lineChart}
         </div>
     )
 }
